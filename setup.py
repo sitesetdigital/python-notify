70976e61ef4a7c9e4776bbe497ca9ac696079369
Alex <alexandergeorgesmith@gmail.com>
2020-01-24 14:50:52 +0000

    setup.py - Include subfolders within package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import setuptools
+from setuptools import setup, find_packages
 
 setuptools.setup(
     name = 'Python Notification package',
@@ -8,6 +8,6 @@ setuptools.setup(
     long_description = "",
     long_description_content_type = "text/plain",
     url = "https://github.com/yui-konnu/python-notify",
-    packages = ['notify'],
+    packages = find_packages(),
     version = '1.1'
 )
