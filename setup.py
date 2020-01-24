657914eaeb844971460f3b5bf02c867a0a5e88e4
yui <yui@madokami.com>
2020-01-24 10:55:25 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+setuptools.setup(
+    name = 'Python Notification package',
+    author = "Siteset Digital",
+    author_email = "yui@madokami.com",
+    description = "",
+    long_description = "",
+    long_description_content_type = "text/plain",
+    url = "https://github.com/yui-konnu/python-notify",
+    packages = ['notify'],
+    version = '1.1'
+)
